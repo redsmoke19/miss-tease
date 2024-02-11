2adab87d71e22363b04dbbd6af95959478e4dc87
Oleg Tsyganov <redsmoke19@gmail.com>
2024-02-11 12:27:13 +0400

    feat: Добавляет блоки Смотреть еще и две страницы

--- source/js/modules/sliders/index.js
+++ source/js/modules/sliders/index.js
@@ -3,6 +3,7 @@ import {initProductSlider} from './init-product.js';
 import {initGallerySlider} from './init-gallery.js';
 import {initHeroSlider} from './init-hero.js';
 import {initInstBlockSlider} from './init-inst-block.js';
+import {initShowMoreSlider} from './init-show-more.js';
 
 const initSliders = () => {
   initHeroSlider();
@@ -10,6 +11,7 @@ const initSliders = () => {
   initGallerySlider();
   initProductSlider();
   initInstBlockSlider();
+  initShowMoreSlider();
 };
 
 export default initSliders;
